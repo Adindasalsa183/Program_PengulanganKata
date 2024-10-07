5bc418b51143c63e1897b9c7791852f4da9c6b2c
Adindasalsa183 <adinda.salsa1207@gmail.com>
2024-10-07 10:59:46 +0700

    Add files via upload

---
+++ Pengulangan Kata.py
@@ -0,0 +1,10 @@
+#Latihan Pengulangan (While)
+
+j = int (input("Masukan Banyaknya Pengulangan Yang Anda Inginkan: "))
+         
+i = 1
+while i < j:
+     print("Selamat Datang")
+     print("Selamat Jalan")
+     print("Selamat Makan")
+     i = i+1
